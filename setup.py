81351fd8aa9a6e888ef863d78ae47bc4f99678fc
Michal Charemza <michal@charemza.name>
2022-09-16 18:04:44 +0100

    build(release): v0.0.71

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='sqlite-s3-query',
-    version='0.0.70',
+    version='0.0.71',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Python context manager to query a SQLite file stored on S3',
